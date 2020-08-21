ea7d40d77ba97d7d86c7ad0363fab41d43965cae
DolotovEvgeniy <dolotov.evgeniy@gmail.com>
2020-08-21 23:34:51 +0300

    Add clean_dataset script

---
+++ clean_dataset.py
@@ -0,0 +1,26 @@
+import os
+from PIL import Image
+import cv2
+import tensorflow as tf
+def clean_dir(dir_path):
+    print('Clean directory: ' + dir_path)
+    count = 0
+    for name in os.listdir(dir_path):
+        path = os.path.join(dir_path, name)
+        print(path)
+        image = cv2.imread(path)
+
+        if tf.compat.as_bytes("JFIF") not in open(path, 'rb').peek(10):
+            os.remove(path)
+            count += 1
+    print('{} images removed'.format(count))
+
+
+if __name__ == '__main__':
+    cat_path = os.path.join('cat-vs-dog/cat')
+    dog_path = os.path.join('cat-vs-dog/dog')
+
+    clean_dir(cat_path)
+    clean_dir(dog_path)
+
+    print('Done!')
