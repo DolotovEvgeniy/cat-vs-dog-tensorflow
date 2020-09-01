e8d31d2333f5af3ed5091fef28cf3b2cf5147cd0
DolotovEvgeniy <dolotov.evgeniy@gmail.com>
2020-09-01 08:50:59 +0300

    Add prepare_datataset.sh script

---
+++ prepare_dataset.sh
@@ -0,0 +1,10 @@
+wget https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip
+unzip kagglecatsanddogs_3367a.zip -d data-tmp
+mkdir cat-vs-dog
+mv data-tmp/PetImages/Dog cat-vs-dog/dog
+mv data-tmp/PetImages/Cat cat-vs-dog/cat
+rm -r kagglecatsanddogs_3367a.zip data-tmp
+
+python clean_dataset.py
+
+python train_test_split.py
