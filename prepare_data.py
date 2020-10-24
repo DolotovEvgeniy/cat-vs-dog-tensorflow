5a47730f7b3d8a14f900d798d9dadaf55d6f8106
DolotovEvgeniy <dolotov.evgeniy@gmail.com>
2020-10-24 21:21:34 +0300

    Data augmentation

--- prepare_data.py
+++ prepare_data.py
@@ -3,7 +3,11 @@ import tensorflow as tf
 def get_datasets():
     # Preprocess the dataset
     train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-        rescale=1.0 / 255.0
+        rescale=1./255,
+        rotation_range=20,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True
     )
 
     train_generator = train_datagen.flow_from_directory('cat-vs-dog/train',
